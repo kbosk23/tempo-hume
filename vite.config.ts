b62fad4255118e3a6940f6ad7c7b8c2a963a1432
kbosk23 <kevin.bobroske@lighthouseaicorp.com>
2025-02-07 19:58:08 +0000

    TS

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5173,
+    port: 3001,
     strictPort: true,
     // @ts-ignore
     allowedHosts: process.env.TEMPO === "true" ? true : undefined,
